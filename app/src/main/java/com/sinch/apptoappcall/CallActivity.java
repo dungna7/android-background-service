3321f57f151c95e33405da1fdfd19ecec7358ddb
phunm128 <phunm128@gmail.com>
2018-08-16 14:13:41 +0900

    ppush code

---
+++ app/src/main/java/com/sinch/apptoappcall/CallActivity.java
@@ -0,0 +1,114 @@
+package com.sinch.apptoappcall;
+
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sinch.android.rtc.PushPair;
+import com.sinch.android.rtc.Sinch;
+import com.sinch.android.rtc.SinchClient;
+import com.sinch.android.rtc.SinchError;
+import com.sinch.android.rtc.calling.Call;
+import com.sinch.android.rtc.calling.CallClient;
+import com.sinch.android.rtc.calling.CallClientListener;
+import com.sinch.android.rtc.calling.CallListener;
+
+import java.util.List;
+
+public class CallActivity extends AppCompatActivity {
+
+    private static final String APP_KEY = "17e6c001-6385-4a9c-b4a1-4f583e706e1f";
+    private static final String APP_SECRET = "aA8GAyzT9EGTJX7XUPkjlA==";
+    private static final String ENVIRONMENT = "sandbox.sinch.com";
+
+    private Call call;
+    private TextView callState;
+    private SinchClient sinchClient;
+    private Button button;
+    private String callerId;
+    private String recipientId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.call);
+        Intent intent = getIntent();
+        callerId = intent.getStringExtra("callerId");
+        recipientId = intent.getStringExtra("recipientId");
+
+        sinchClient = Sinch.getSinchClientBuilder()
+                .context(this)
+                .userId("b")
+                .applicationKey(APP_KEY)
+                .applicationSecret(APP_SECRET)
+                .environmentHost(ENVIRONMENT)
+                .build();
+
+        sinchClient.setSupportCalling(true);
+        sinchClient.startListeningOnActiveConnection();
+        sinchClient.start();
+
+        sinchClient.getCallClient().addCallClientListener(new SinchCallClientListener());
+
+        button = (Button) findViewById(R.id.button);
+        callState = (TextView) findViewById(R.id.callState);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (call == null) {
+                    call = sinchClient.getCallClient().callUser("a");
+                    call.addCallListener(new SinchCallListener());
+                    button.setText("Hang Up");
+                } else {
+                    call.hangup();
+                }
+            }
+        });
+    }
+
+    private class SinchCallListener implements CallListener {
+        @Override
+        public void onCallEnded(Call endedCall) {
+            call = null;
+            SinchError a = endedCall.getDetails().getError();
+            button.setText("Call");
+            callState.setText("");
+            setVolumeControlStream(AudioManager.USE_DEFAULT_STREAM_TYPE);
+        }
+
+        @Override
+        public void onCallEstablished(Call establishedCall) {
+            callState.setText("connected");
+            setVolumeControlStream(AudioManager.STREAM_VOICE_CALL);
+        }
+
+        @Override
+        public void onCallProgressing(Call progressingCall) {
+            callState.setText("ringing");
+        }
+
+        @Override
+        public void onShouldSendPushNotification(Call call, List<PushPair> pushPairs) {
+        }
+    }
+
+    private class SinchCallClientListener implements CallClientListener {
+        @Override
+        public void onIncomingCall(CallClient callClient, Call incomingCall) {
+            call = incomingCall;
+            Toast.makeText(CallActivity.this, "incoming call", Toast.LENGTH_SHORT).show();
+            call.answer();
+            call.addCallListener(new SinchCallListener());
+            button.setText("Hang Up");
+        }
+    }
+}
+
